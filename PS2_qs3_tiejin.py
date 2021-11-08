5716954733870aaaac907f6337dcccec801ef8be
tiejin <tiejin@umich.edu>
2021-11-07 23:39:54 -0500

    update with PS4's code

--- PS2_qs3_tiejin.py
+++ PS2_qs3_tiejin.py
@@ -4,23 +4,59 @@ import time
 import re
 import warnings
 
+
 sas_list = ["Demo_G.XPT","DEMO_H.XPT","DEMO_I.XPT","DEMO_J.XPT"]
+demo_cat = {
+    'gender': {1: 'Male', 2: 'Female'},
+    'race': {1: 'Mexican American',
+             2: 'Other Hispanic',
+             3: 'Non-Hispanic White',
+             4: 'Non-Hispanic Black',
+             6: 'Non-Hispanic Asian',
+             7: 'Other/Multiracial'
+             },
+    'education': {1: 'Less than 9th grade',
+                  2: '9-11th grade (Includes 12th grade with no diploma)',
+                  3: 'High school graduate/GED or equivalent',
+                  4: 'Some college or AA degree',
+                  5: 'College graduate or above',
+                  7: 'Refused',
+                  9: "Don't know"
+                  },
+    'marital_status': {1: 'Married',
+                       2: 'Widowed',
+                       3: 'Divorced',
+                       4: 'Separated',
+                       5: 'Never married',
+                       6: 'Living with partner',
+                       77: 'Refused',
+                       99: "Don't know"
+                       },
+    'exam_status': {1: 'Interviewed only',
+                    2: 'Both interviewed and MEC examined'
+                    }
+    }
 res = []
 for i in range(len(sas_list)):
-    name_list = ['SEQN','RIDAGEYR','RIDRETH3','DMDEDUC2','DMDMARTL',
+    name_list = ['SEQN','RIDAGEYR','RIAGENDR','RIDRETH3','DMDEDUC2','DMDMARTL',
                  'RIDSTATR','SDMVPSU','SDMVSTRA','WTMEC2YR','WTINT2YR']
     df = pd.read_sas(sas_list[i])[name_list].convert_dtypes()
     df['RIDAGEYR'] = df['RIDAGEYR'].astype("int64")
-    df.columns = ['UniqueID','Age','Race and Ethnicity','Education',' Marital Status',
-                  'Examination Status','Pseudo-PSU','Pseudo-Stratum','MEC weight','Interview weight']
+    df.columns = ['UniqueID','age','gender','race','education','marital_status',
+                  'exam_status','Pseudo-PSU','Pseudo-Stratum','MEC weight','Interview weight']
     df.set_index('UniqueID',inplace=True)
     df['cohort'] = i
     res.append(df)
 res = pd.concat(res)
 res['cohort'] = pd.Categorical(res['cohort'].replace({0:"2011-2012",1:"2013-2014",
                                                     2:"2015-2016",3:"2017-2018"}))
+cate_col = res.columns[1:6]
+for col_name in cate_col:
+    res[col_name] = pd.Categorical(res[col_name].replace(demo_cat[col_name]))
+
 print(res)
 res.to_pickle("demographic data.pickle")
+
 
 # #### Part b
 # We do the same thing as **Part a**. However, we do not know the exact characters in OHXxxTC and OHXxxCTC.
