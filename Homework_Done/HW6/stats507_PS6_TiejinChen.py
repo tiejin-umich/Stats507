11e1e59bf142bd402ef990767a6b1a6feaf2e53b
tiejin <tiejin@umich.edu>
2021-11-09 12:45:35 -0500

    HW6 update

---
+++ Homework_Done/HW6/stats507_PS6_TiejinChen.py
@@ -0,0 +1,77 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.12.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Problem Set 6
+# ### Question 0
+# The link to my repo is:https://github.com/tiejin-umich/Stats507 or
+# [here](https://github.com/tiejin-umich/Stats507)
+# We put all the homework we done in the folder ```Homework_Done```
+# And put all other files in the main folder.
+
+# ### Question 1
+# To run the code, it needs data. Hence here we also push data file to the main folder.
+# However, We might not push these files.
+# Hence we do not store the command line we use to push these files.
+# And here is the command line we use:
+#
+#     #Extract your code as PS2_qs3_tiejin.py.
+#     git add PS2_qs3_tiejin.py
+#     #Edit my README.md.
+#     git add README.md
+#     git commit -m "PS6_Q1_first_commit"
+#     git push
+#     git branch ps4
+#     git checkout ps4
+#     #Edit the script.
+#     git add PS2_qs3_tiejin.py
+#     git commit -m "update with PS4's code"
+#     git push --set-upstream origin ps4
+#     git checkout main
+#     git merge ps4
+#     git push
+#
+#
+
+# We have the following link to show:
+# 1. in step3, we have the commit:
+# https://github.com/tiejin-umich/Stats507/commit/eaa892d7e3f5a0b8272eed1669eaf079244545a0
+# 2. in step5, we have the commit:
+# https://github.com/tiejin-umich/Stats507/commit/5716954733870aaaac907f6337dcccec801ef8be
+
+# ### Question 2
+# The link to file we uploads is:
+# https://github.com/tiejin-umich/Stats507/blob/main/pandas_notes/pd_topic_tiejin.py
+
+# ### Question 3
+#
+# In Question 3, We want to correct PS1 and PS5
+
+# For PS1, we have the following comment:
+#
+# **Q0: -3 for don't provide both raw and formatted versions**
+#
+# We want to correct this error, and the commit correction link is:
+# https://github.com/tiejin-umich/Stats507/commit/3f510bc817a8ba591de288c0e216e7496f66a9eb
+
+# For PS5, we have not lost our points yet.
+# However, we find we forget to polish our writing style
+# such as forget to write docstring for a function
+# or the length of a line is too long.
+# And we will lose our points absolutely. Hence we want to correct PS5 Here.
+#
+# And the commit correction link is:
+# https://github.com/tiejin-umich/Stats507/commit/e5442ab2a144fb09378f28b0327ca5328801d2fd
+
+
